8a35e0d17007ed89f050e1dfc76ad202055476a6
kuliahumy26-byte <kuliahumy26@gmail.com>
2026-04-28 20:08:29 +0700

    membuat class layang layang

---
+++ Praktikum_FriendClass&Pointer_0142.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class layangLayang{
+    private:
+    int diagSatu, diagDua, sisiPendek, sisiPanjang, hasilLuas, hasilKeliling;
+
+    public:
+    void luas(){
+        hasilLuas = 0.5 * diagSatu * diagDua;
+    }
+
+    void keliling(){
+        hasilKeliling = sisiPendek * sisiPendek * sisiPanjang * sisiPanjang;
+    }
+
+    void input(){
+        cout << "Masukan diagonal ke-1: ";
+        cin >> diagSatu;
+        cout << "Masukan diagonal ke-2: ";
+        cin >> diagDua;
+
+        cout << "Masukan sisi pendek: ";
+        cin >> sisiPendek;
+        cout << "Masukan sisi panjang: ";
+        cin >> sisiPanjang;
+    }
+    void output(){
+        cout << "hasil luas: " << hasilLuas << endl;
+        cout << "hasil keliling: " << hasilKeliling << endl;
+    }
+
+    friend class belahKetupat;
+};
+
