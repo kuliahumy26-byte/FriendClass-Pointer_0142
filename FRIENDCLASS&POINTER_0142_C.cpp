5bcfb467c6db267482cb10f0d7ee8196784b05e5
kuliahumy26-byte <kuliahumy26@gmail.com>
2026-04-28 18:46:20 +0700

    membuat fungsi implementasi

--- FRIENDCLASS&POINTER_0142_C.cpp
+++ FRIENDCLASS&POINTER_0142_C.cpp
@@ -21,3 +21,12 @@ public:
     void setId(int pId);
     void displayAll(orang &a);
 };
+
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl << a.nama;
+}
+void orang::setNama(string pNama)
+{
+    nama = pNama;
+}
